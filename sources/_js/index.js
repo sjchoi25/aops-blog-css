67658883dd24a97276c61298fd3c19e1af15a570
Nhv24 <69376044+Nhv24@users.noreply.github.com>
2021-04-03 14:57:43 -0600

    Update index.js

--- sources/_js/index.js
+++ sources/_js/index.js
@@ -1,18 +1,23 @@
+// Code by Nhv24 (github)/player01 (AoPS)/player010 (replit)
+
 const app = require("express")();
 const http = require('http');
 const server = http.createServer(app);
 
+// Url for testing
 app.get("/test.css", (req, res) => {
 	console.log("test.css requested");
 	res.setHeader('Content-Type', 'text/css');
   	res.send("body {color: blue;}");
 });
 
+// Wakeup url
 app.get("/wakeup", (req, res) => {
 	console.log("Waking server...\nDone.");
 	res.send("Waking up...");
 })
 
+// Base url
 app.get("/", (req, res) => {
 	console.log("Requested");
 	res.send("Hey");
